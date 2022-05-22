0b679c6b2af580af21b96c9d20f8cec25108a526
Cullen <cullen.dolan@gmail.com>
2022-05-22 10:31:35 -0500

    testing argparse

---
+++ myls.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import argparse
+
+# Create the parser
+my_parser = argparse.ArgumentParser(description='List content of folder')
+
+# add args
+my_parser.add_argument("Path",
+                        metavar="path",
+                        type=str,
+                        help="the path to list")
+
+# execute the method
+args = my_parser.parse_args()
+
+input_path = args.Path
+
+if not os.path.isdir(input_path):
+    print("Path doesn't exist")
+    sys.exit()
+
+print('\n'.join(os.listdir(input_path)))
+
