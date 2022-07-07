269fec1d6b616d4eade96cefe873b7f810343997
Cullen <cullen.dolan@gmail.com>
2022-07-07 10:35:53 -0500

    add coopint finction

--- txtprocessor.py
+++ txtprocessor.py
@@ -1,3 +1,4 @@
+from tkinter import filedialog
 import pandas as pd
 import argparse
 
@@ -11,23 +12,23 @@ my_parser.add_argument("InvoiceType",
                         nargs='?',
                         help='StarInterchange, COOP, CardPro')
 
-my_parser.add_argument("InvoiceDate",
-                        nargs='?',
-                        help='format = YYYY-MM-DD')
-
 my_parser.add_argument("FileDate",
                         nargs='?',
-                        help='format = YYYY-MM-DD')
+                        help='File Date format = YYYY-MM-DD')
+
+my_parser.add_argument("InvoiceDate",
+                        nargs='?',
+                        help='Invoice Date format = YYYY-MM-DD')
 
 #file_date = '2022-6-1'
 #invoice_date = '2022-5-1'
 
 args = my_parser.parse_args()
-
 input_path = args.Path.lower()
 invoice_type = args.InvoiceType.lower()
 file_date = args.FileDate
 invoice_date = args.InvoiceDate
+output_filename = 'fullcoopintfile.csv'#invoice_type + '_' + file_date + '.csv'
 
 def process_star_int(df,file_date,invoice_date):
     try:
@@ -35,17 +36,19 @@ def process_star_int(df,file_date,invoice_date):
         df = df.drop(['Unnamed: 8'], axis = 1)
         df['FileDate'] = file_date
         df['InvoiceDate'] = invoice_date
-        df.to_csv('output.csv', mode = 'a', index = False) # mode = a appends date to file
+        df.to_csv(output_filename, mode = 'a', index = False) # mode = a appends date to file
     except:
         print('no header')
         df['FileDate'] = file_date
         df['InvoiceDate'] = invoice_date
-        df.to_csv('output.csv', mode = 'a', index = False) 
+        df.to_csv(output_filename, mode = 'a', index = False) 
 
 def process_coop_int(df,file_date,invoice_date):
     df['BILL'] = df['BILL'] + df['Unnamed: 3']
     df = df.drop(['Unnamed: 3'], axis = 1)
-    df.to_csv('output.csv', mode = 'a', index = False) 
+    df['FileDate']  = file_date
+    df['InvoiceDate'] = invoice_date
+    df.to_csv(output_filename, mode = 'a', index = False) 
 
 if invoice_type == 'starint':
     df = pd.read_fwf(input_path)
